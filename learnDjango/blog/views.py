448c822cd644fab1e198b1ec2a2f462f33094c15
dahn510 <dahn510@gmail.com>
2021-06-03 01:36:17 -0700

    CRUD on blog posts

--- learnDjango/blog/views.py
+++ learnDjango/blog/views.py
@@ -1,11 +1,56 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
-def home(request):
-    context = {
-            'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # overriding form_valid view to set the author as logged in user 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+
+        # now run the parent form_valid() funtion and pass in this form
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # overriding form_valid view to set the author as logged in user 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+
+        # now run the parent form_valid() funtion and pass in this form
+        return super().form_valid(form)
+
+    # test whether the currently logged in user is same as post author
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
     return render(request, 'blog/about.html')
